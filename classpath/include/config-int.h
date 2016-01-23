af301689bf38c4727e40b94b95c31a3bbe970a33
Valentin Ilie <valentin.ilie@gmail.com>
2016-01-23 16:27:51 +0200

    classpath: Add classpath 0.99
    Signed-off-by: Valentin Ilie <valentin.ilie@gmail.com>

---
+++ classpath/include/config-int.h
@@ -0,0 +1,9 @@
+#ifndef _CLASSPATH_INCLUDE_CONFIG_INT_H
+#define _CLASSPATH_INCLUDE_CONFIG_INT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "classpath 0.99"
+/* generated using gnu compiler gcc (Gentoo 4.6.2) 4.6.2 */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
