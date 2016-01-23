8f0d304a3f13e298fef8afa6eab359a4aa077bfe
Valentin Ilie <valentin.ilie@gmail.com>
2016-01-23 21:07:09 +0200

    classpath: Remove emconfigure, build it with CC=emcc arg
    Signed-off-by: Valentin Ilie <valentin.ilie@gmail.com>

--- classpath/javify-build.sh
+++ classpath/javify-build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-emconfigure ./configure
+
+# TODO: Make this work with 'emconfigure'. It doesn't find gconf2 even it is present.
+# emconfigure ./configure
+
+./configure CC=emcc --disable-gtk-peer
 
