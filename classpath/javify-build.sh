4f25b24f38ba220917bc38b5b7d5710da83cb6d4
Valentin Ilie <valentin.ilie@gmail.com>
2016-01-24 00:02:19 +0200

    classpath: Add --disable-gjdoc flag to config
    Signed-off-by: Valentin Ilie <valentin.ilie@gmail.com>

--- classpath/javify-build.sh
+++ classpath/javify-build.sh
@@ -3,5 +3,5 @@
 # TODO: Make this work with 'emconfigure'. It doesn't find gconf2 even it is present.
 # emconfigure ./configure
 
-./configure CC=emcc --disable-gtk-peer
+./configure CC=emcc --disable-gtk-peer --disable-gjdoc
 
