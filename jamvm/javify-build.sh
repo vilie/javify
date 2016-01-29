a76851166d969bc791d8d674b544e7081d707367
Valentin Ilie <valentin.ilie@gmail.com>
2016-01-29 13:05:17 +0200

    jamvm: javify-build.sh: Copy all .so files to a folder
    Signed-off-by: Valentin Ilie <valentin.ilie@gmail.com>

--- jamvm/javify-build.sh
+++ jamvm/javify-build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 emconfigure ./configure --disable-zip --host=i386-linux
 emmake make
+
+mkdir lib
+find ../classpath -name \*.so -exec cp {} ./lib \;
+find . -name \*.so -exec cp {} ./lib \;
